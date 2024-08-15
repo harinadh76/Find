08479b8e9dccbbddc190c228e828643465debd7e
harinadh76 <tannaharinadh@gmail.com>
2024-08-15 19:48:54 +0530

    added details component

---
+++ findCook/mobile/components/Cards/Error.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Error = () => {
+    return (
+        <View>
+            <Text>You are Not authorized to View this Page</Text>
+        </View>
+    )
+}
+
+export default Error
+
+const styles = StyleSheet.create({})
