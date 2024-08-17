42bc31eb8ab28d02c8db62e9a6c6fbeb22720431
harinadh76 <tannaharinadh@gmail.com>
2024-08-17 17:07:45 +0530

    added details page

--- findCook/mobile/app/Details/index.tsx
+++ findCook/mobile/app/Details/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
 import React, { useEffect, useState } from 'react'
 // import CookDetails from '@/components/Cards/CookDetails'
 // import { router, useRouter } from 'expo-router';
@@ -8,14 +8,21 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Error from '@/components/Cards/Error';
 import NoData from '@/components/Cards/NoData';
+import { cookData } from '@/constants/CookData';
 
 
 const index = () => {
     const [token, setToken] = useState('');
-    const [cook, setCook] = useState({});
+    const [cook, setCook] = useState<any>({});
     const params = useLocalSearchParams();
 
     useEffect(() => {
+        setCook({
+            ...cook,
+            ...cookData[0]
+        })
+        // console.log('cook', cookData[0]);
+        // console.log('____', cook);
         AsyncStorage.getItem('token').then((token) => {
             if (!token) {
                 <Error />
@@ -31,18 +38,36 @@ const index = () => {
                     }
                 }).then((res) => {
                     if (res.data) {
-                        setCook(res.data);
+                        // setCook(res.data);
                     }
                 }).catch((err) => console.log(err));
             }
         });
     }, [])
 
+    useEffect(() => {
+        console.log('cook', cook);
+    }, [cook])
+
     return (
         <View>
             {/* {!Object.values(cook).length ? <NoData /> : <Text> Cook Details card</Text>} */}
+            <Image source={require('@/assets/images/react-logo.png')} />
+            <Text>{cook.name}</Text>
+            <Text>{cook.phone}</Text>
+            <Text>{cook.email}</Text>
+            <Text>{cook.address}</Text>
+            <Text>{cook.city}</Text>
+            <Text>{cook.state}</Text>
+            <Text>{cook.type}</Text>
+            <Text>{cook.pricePerMonth}</Text>
+            <Text>{cook.pricePerService}</Text>
+            <Text>{cook.cookRating / cook.cookRatingCount}</Text>
+            <Text>{cook.description}</Text>
 
-            <Text> Cook Details card</Text>
+            <Button title='show number' />
+            <Button title='chat' />
+            <Button title='hire' />
         </View>
     )
 }
